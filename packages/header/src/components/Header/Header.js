9e7379724eb036cc972997254cbef260e445c840
Guilherme Mangabeira Gregio <guilherme@3gweb.com.br>
2019-06-12 17:31:29 -0300

    feat: add header

---
+++ packages/header/src/components/Header/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {css} from '@emotion/core';
+import styled from '@emotion/styled';
+
+const headerBase = css`
+  display: flex;
+  align-items: center;
+  background: #1d8;
+  color: #fff;
+  padding: 0 5px;
+`;
+
+const headerBrandBase = css`
+  flex: 1 1 auto;
+  padding: 10px;
+  text-align: center;
+`;
+
+const HeaderWrap = styled.header`
+  ${headerBase}
+`;
+
+const HeaderBrand = styled.h1`
+  ${headerBrandBase}
+`;
+
+const Header = () => (
+  <HeaderWrap>
+    <div>X</div>
+    <HeaderBrand>Creditas</HeaderBrand>
+    <div>©</div>
+  </HeaderWrap>
+);
+
+export {Header};
