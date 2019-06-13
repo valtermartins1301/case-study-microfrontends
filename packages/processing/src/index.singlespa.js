971695d632febe4126f58e1f8edd2ed210692dac
Guilherme Mangabeira Gregio <guilherme@3gweb.com.br>
2019-06-13 16:24:13 -0300

    chore: add build to processing

---
+++ packages/processing/src/index.singlespa.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import singleSpaReact from 'single-spa-react'
+import {App} from './App'
+
+const reactLifecycles = singleSpaReact({
+  React,
+  ReactDOM,
+  rootComponent: App,
+  domElementGetter,
+})
+
+const bootstrap = [reactLifecycles.bootstrap]
+
+const mount = [reactLifecycles.mount]
+
+const unmount = [reactLifecycles.unmount]
+
+function domElementGetter() {
+  return document.getElementById('processing')
+}
+
+export {bootstrap, mount, unmount}
